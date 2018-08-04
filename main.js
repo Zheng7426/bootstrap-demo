b5736a4fec83bc06a26c1d75ae6b4d749f0f2d30
zheng7426 <lilsam6670@gmail.com>
2018-08-04 20:14:33 +0800

    first & probably last commit

---
+++ main.js
@@ -0,0 +1,13 @@
+//Header page
+$(document).ready(function(){
+	$('.header').height($(window).height());
+})
+
+//Scroll Effect
+$(document).ready(function(){
+	$('.navbar a').click(function() {
+		$("body, html").animate({
+			scrollTop:$("#" + $(this).data('value')).offset().top
+		}, 1000)
+	})
+})
